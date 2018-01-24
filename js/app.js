7ffd503c5cd95de0f868ec2e049ae77663fa8dbf
Josefina Mancilla <josefinanoemi4@gmail.com>
2018-01-24 17:59:33 -0600

    updated particle styling

--- js/app.js
+++ js/app.js
@@ -11,123 +11,112 @@ particlesJS.load('particles-js', 'particles.json', function() {
 
 /* Otherwise just put the config content (json): */
 
-particlesJS('particles-js',
-
+particlesJS("particles-js",
   {
-    "particles": {
-      "number": {
-        "value": 80,
-        "density": {
-          "enable": true,
-          "value_area": 800
+    "particles":{
+      "number":{
+        "value":147,
+        "density":{
+          "enable":true,
+          "value_area":800
         }
       },
-      "color": {
-        "value": "#ffffd9"
+      "color":{
+        "value":"#ffffd9"
       },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
+      "shape":{
+        "type":"circle",
+        "stroke":{
+          "width":0,
+          "color":"#ffffff"
         },
-        "polygon": {
-          "nb_sides": 5
+        "polygon":{
+          "nb_sides":3
         },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
+        "image":{
+          "src":"img/github.svg"
+          ,"width":100,
+          "height":100
         }
       },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
+      "opacity":{
+        "value":0.3787908679834909,
+        "random":false,
+        "anim":{
+          "enable":false,
+          "speed":1,
+          "opacity_min":0.1,
+          "sync":false
         }
       },
-      "size": {
-        "value": 5,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
+      "size":{
+        "value":3,
+        "random":true,
+        "anim":{
+          "enable":false,
+          "speed":23.976023976023978,
+          "size_min":0.1,"sync":false
         }
       },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#edf8b1",
-        "opacity": 0.4,
-        "width": 1
+      "line_linked":{
+        "enable":true,
+        "distance":150,
+        "color":"#edf8b1",
+        "opacity":0.48138006139568634,
+        "width":1
       },
-      "move": {
-        "enable": true,
-        "speed": 3,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
+      "move":{
+        "enable":true,
+        "speed":3,
+        "direction":"none",
+        "random":false,
+        "straight":false,
+        "out_mode":"out",
+        "bounce":false,
+        "attract":{
+          "enable":false,
+          "rotateX":600,
+          "rotateY":1200
         }
       }
     },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "repulse"
+    "interactivity":{
+      "detect_on":"canvas",
+      "events":{
+        "onhover":{
+          "enable":true,
+          "mode":"repulse"
         },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
+        "onclick":{
+          "enable":true,
+          "mode":"push"
         },
-        "resize": true
+        "resize":true
       },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
+      "modes":{
+        "grab":{
+          "distance":400,
+          "line_linked":{
+            "opacity":1
           }
         },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
+        "bubble":{
+          "distance":400,
+          "size":40,
+          "duration":2,
+          "opacity":8,
+          "speed":3
         },
-        "repulse": {
-          "distance": 200
+        "repulse":{
+          "distance":200,
+          "duration":0.4
         },
-        "push": {
-          "particles_nb": 4
+        "push":{
+          "particles_nb":4
         },
-        "remove": {
-          "particles_nb": 2
+        "remove":{
+          "particles_nb":2
         }
       }
     },
-    "retina_detect": true,
-    "config_demo": {
-      "hide_card": false,
-      "background_color": "#090b20",
-      "background_image": "",
-      "background_position": "50% 50%",
-      "background_repeat": "no-repeat",
-      "background_size": "cover"
-    }
-  }
-
-);
+    "retina_detect":true});
